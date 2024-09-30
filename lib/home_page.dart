b58bea718474d50659ccf1f9c1b9adf9c67a2d90
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2024-09-30 09:09:48 -0300

    feat: request android storage permission

---
+++ lib/home_page.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final Permission _permission = Permission.storage;
+  PermissionStatus _permissionStatus = PermissionStatus.denied;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('PoC PDF'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("${_permission.toString()} : ${_permissionStatus.toString()}"),
+            const SizedBox(height: 8),
+            FilledButton(
+              onPressed: _requestPermission,
+              child: const Text("Request storage permission"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _listenForPermissionStatus();
+  }
+
+  void _listenForPermissionStatus() async {
+    final status = await _permission.status;
+
+    setState(() => _permissionStatus = status);
+  }
+
+  Future<void> _requestPermission() async {
+    final status = await _permission.request();
+
+    setState(() {
+      _permissionStatus = status;
+    });
+  }
+}
